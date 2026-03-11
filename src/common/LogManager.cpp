f316f145ae6ebd78b71a35314157fbf72d753b72
Adel <adel.elhassani@gmail.com>
2026-03-11 03:53:03 +0100

    Had merged the wrong version of LogManager.cpp, now fixed.

--- src/common/LogManager.cpp
+++ src/common/LogManager.cpp
@@ -21,11 +21,19 @@ LogManager::~LogManager() {
 }
 
 bool LogManager::Init() {
+	time_t t;
+	char dateString[51];
+
+	t = time(NULL);
+	tzset();
+	strftime(dateString, sizeof(dateString) - 1, "%a %b %d %T %Z %Y.txt", localtime(&t));
+
+	m.logFile = fopen(dateString, "w");
 	return true;
 }
 
 void LogManager::Shutdown() {
-	
+	fclose(m.logFile);
 }
 
 void LogManager::Log(LOG_LEVEL level, const char* fmt, ...) {
